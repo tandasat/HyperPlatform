9cc8d4e0c9283d56f2fe25596ad681a9adf42a93
tandasat <tanda.sat@gmail.com>
2016-05-08 18:20:40 -0700

    fix #17

--- HyperPlatform/util.cpp
+++ HyperPlatform/util.cpp
@@ -27,74 +27,6 @@ extern "C" {
 // futher investigation.
 static const auto kUtilpUseRtlPcToFileHeader = false;
 
-#if defined(_AMD64_)
-
-// Base addresses of page structures. Use !pte to obtain them.
-static const auto kUtilpPxeBase = 0xfffff6fb7dbed000ull;
-static const auto kUtilpPpeBase = 0xfffff6fb7da00000ull;
-static const auto kUtilpPdeBase = 0xfffff6fb40000000ull;
-static const auto kUtilpPteBase = 0xfffff68000000000ull;
-
-// Get the highest 25 bits
-static const auto kUtilpPxiShift = 39ull;
-
-// Get the highest 34 bits
-static const auto kUtilpPpiShift = 30ull;
-
-// Get the highest 43 bits
-static const auto kUtilpPdiShift = 21ull;
-
-// Get the highest 52 bits
-static const auto kUtilpPtiShift = 12ull;
-
-// Use  9 bits; 0b0000_0000_0000_0000_0000_0000_0001_1111_1111
-static const auto kUtilpPxiMask = 0x1ffull;
-
-// Use 18 bits; 0b0000_0000_0000_0000_0011_1111_1111_1111_1111
-static const auto kUtilpPpiMask = 0x3ffffull;
-
-// Use 27 bits; 0b0000_0000_0111_1111_1111_1111_1111_1111_1111
-static const auto kUtilpPdiMask = 0x7ffffffull;
-
-// Use 36 bits; 0b1111_1111_1111_1111_1111_1111_1111_1111_1111
-static const auto kUtilpPtiMask = 0xfffffffffull;
-
-#elif defined(_X86_)
-
-// Base addresses of page structures. Use !pte to obtain them.
-static const auto kUtilpPdeBase = 0xc0300000;
-static const auto kUtilpPteBase = 0xc0000000;
-
-// Get the highest 10 bits
-static const auto kUtilpPdiShift = 22;
-
-// Get the highest 20 bits
-static const auto kUtilpPtiShift = 12;
-
-// Use 10 bits; 0b0000_0000_0000_0000_0000_0000_0011_1111_1111
-static const auto kUtilpPdiMask = 0x3ff;
-
-// Use 20 bits; 0b0000_0000_0000_0000_1111_1111_1111_1111_1111
-static const auto kUtilpPtiMask = 0xfffff;
-
-#endif
-
-// Base addresses of page structures. Use !pte to obtain them.
-static const auto kUtilpPdeBasePae = 0xc0600000;
-static const auto kUtilpPteBasePae = 0xc0000000;
-
-// Get the highest 11 bits
-static const auto kUtilpPdiShiftPae = 21;
-
-// Get the highest 20 bits
-static const auto kUtilpPtiShiftPae = 12;
-
-// Use 11 bits; 0b0000_0000_0000_0000_0000_0000_0111_1111_1111
-static const auto kUtilpPdiMaskPae = 0x7ff;
-
-// Use 20 bits; 0b0000_0000_0000_0000_1111_1111_1111_1111_1111
-static const auto kUtilpPtiMaskPae = 0xfffff;
-
 ////////////////////////////////////////////////////////////////////////////////
 //
 // types
@@ -135,6 +67,9 @@ struct LdrDataTableEntry {
 //
 
 _IRQL_requires_max_(PASSIVE_LEVEL) static NTSTATUS
+    UtilpInitializePageTableVariables();
+
+_IRQL_requires_max_(PASSIVE_LEVEL) static NTSTATUS
     UtilpInitializeRtlPcToFileHeader(_In_ PDRIVER_OBJECT driver_object);
 
 _Success_(return != nullptr) static PVOID NTAPI
@@ -148,23 +83,18 @@ _IRQL_requires_max_(PASSIVE_LEVEL) static PhysicalMemoryDescriptor
 
 static bool UtilpIsCanonicalFormAddress(_In_ void *address);
 
-#if defined(_AMD64_)
 static HardwarePte *UtilpAddressToPxe(_In_ const void *address);
 
 static HardwarePte *UtilpAddressToPpe(_In_ const void *address);
-#endif
 
 static HardwarePte *UtilpAddressToPde(_In_ const void *address);
 
 static HardwarePte *UtilpAddressToPte(_In_ const void *address);
 
-static HardwarePte *UtilpAddressToPdePAE(_In_ const void *address);
-
-static HardwarePte *UtilpAddressToPtePAE(_In_ const void *address);
-
 #if defined(ALLOC_PRAGMA)
 #pragma alloc_text(INIT, UtilInitialization)
 #pragma alloc_text(PAGE, UtilTermination)
+#pragma alloc_text(INIT, UtilpInitializePageTableVariables)
 #pragma alloc_text(INIT, UtilpInitializeRtlPcToFileHeader)
 #pragma alloc_text(INIT, UtilpInitializePhysicalMemoryRanges)
 #pragma alloc_text(INIT, UtilpBuildPhysicalMemoryRanges)
@@ -186,6 +116,21 @@ static PhysicalMemoryDescriptor *g_utilp_physical_memory_ranges;
 static MmAllocateContiguousNodeMemoryType
     *g_utilp_MmAllocateContiguousNodeMemory;
 
+static ULONG_PTR g_utilp_pxe_base = 0;
+static ULONG_PTR g_utilp_ppe_base = 0;
+static ULONG_PTR g_utilp_pde_base = 0;
+static ULONG_PTR g_utilp_pte_base = 0;
+
+static ULONG_PTR g_utilp_pxi_shift = 0;
+static ULONG_PTR g_utilp_ppi_shift = 0;
+static ULONG_PTR g_utilp_pdi_shift = 0;
+static ULONG_PTR g_utilp_pti_shift = 0;
+
+static ULONG_PTR g_utilp_pxi_mask = 0;
+static ULONG_PTR g_utilp_ppi_mask = 0;
+static ULONG_PTR g_utilp_pdi_mask = 0;
+static ULONG_PTR g_utilp_pti_mask = 0;
+
 ////////////////////////////////////////////////////////////////////////////////
 //
 // implementations
@@ -196,7 +141,15 @@ _Use_decl_annotations_ NTSTATUS
 UtilInitialization(PDRIVER_OBJECT driver_object) {
   PAGED_CODE();
 
-  auto status = UtilpInitializeRtlPcToFileHeader(driver_object);
+  auto status = UtilpInitializePageTableVariables();
+  HYPERPLATFORM_LOG_DEBUG("PXE at %p, PPE at %p, PDE at %p, PTE at %p",
+                          g_utilp_pxe_base, g_utilp_ppe_base, g_utilp_pde_base,
+                          g_utilp_pte_base);
+  if (!NT_SUCCESS(status)) {
+    return status;
+  }
+
+  status = UtilpInitializeRtlPcToFileHeader(driver_object);
   if (!NT_SUCCESS(status)) {
     return status;
   }
@@ -220,6 +173,103 @@ _Use_decl_annotations_ void UtilTermination() {
     ExFreePoolWithTag(g_utilp_physical_memory_ranges,
                       kHyperPlatformCommonPoolTag);
   }
+}
+
+// Initializes g_utilp_p*e_base, g_utilp_p*i_shift and g_utilp_p*i_mask.
+_Use_decl_annotations_ static NTSTATUS UtilpInitializePageTableVariables() {
+  PAGED_CODE();
+
+#include "util_page_constants.h"  // Include platform dependent constants
+
+  // Check OS version to know if page table base addresses need to be relocated
+  RTL_OSVERSIONINFOW os_version = {sizeof(os_version)};
+  auto status = RtlGetVersion(&os_version);
+  if (!NT_SUCCESS(status)) {
+    return status;
+  }
+
+  // Win 10 build 14316 is the first version implements randomized page tables
+  if (os_version.dwMajorVersion < 10 || os_version.dwBuildNumber < 14316) {
+    if (IsX64()) {
+      g_utilp_pxe_base = kUtilpPxeBase;
+      g_utilp_ppe_base = kUtilpPpeBase;
+      g_utilp_pxi_shift = kUtilpPxiShift;
+      g_utilp_ppi_shift = kUtilpPpiShift;
+      g_utilp_pxi_mask = kUtilpPxiMask;
+      g_utilp_ppi_mask = kUtilpPpiMask;
+    }
+    if (UtilIsX86Pae()) {
+      g_utilp_pde_base = kUtilpPdeBasePae;
+      g_utilp_pte_base = kUtilpPteBasePae;
+      g_utilp_pdi_shift = kUtilpPdiShiftPae;
+      g_utilp_pti_shift = kUtilpPtiShiftPae;
+      g_utilp_pdi_mask = kUtilpPdiMaskPae;
+      g_utilp_pti_mask = kUtilpPtiMaskPae;
+    } else {
+      g_utilp_pde_base = kUtilpPdeBase;
+      g_utilp_pte_base = kUtilpPteBase;
+      g_utilp_pdi_shift = kUtilpPdiShift;
+      g_utilp_pti_shift = kUtilpPtiShift;
+      g_utilp_pdi_mask = kUtilpPdiMask;
+      g_utilp_pti_mask = kUtilpPtiMask;
+    }
+    return status;
+  }
+
+  if (!IsX64()) {
+    HYPERPLATFORM_LOG_ERROR(
+        "Unsupported OS version was detected. Detected version %d.%d.%d.",
+        os_version.dwMajorVersion, os_version.dwMinorVersion,
+        os_version.dwBuildNumber);
+    HYPERPLATFORM_LOG_ERROR(
+        "Reason: Need of page table address relacation on x86 Win10 RS is not "
+        "investigated.");
+    return STATUS_UNSUCCESSFUL;
+  }
+
+  // Get PTE_BASE from MmGetVirtualForPhysical
+  const auto p_MmGetVirtualForPhysical =
+      UtilGetSystemProcAddress(L"MmGetVirtualForPhysical");
+  if (!p_MmGetVirtualForPhysical) {
+    return STATUS_PROCEDURE_NOT_FOUND;
+  }
+
+  static const UCHAR kPatternWin10x64[] = {
+      0x48, 0x8b, 0x04, 0xd0,  // mov     rax, [rax+rdx*8]
+      0x48, 0xc1, 0xe0, 0x19,  // shl     rax, 19h
+      0x48, 0xba,              // mov     rdx, ????????`????????  ; PTE_BASE
+  };
+  auto found = reinterpret_cast<ULONG_PTR>(
+      UtilMemMem(p_MmGetVirtualForPhysical, 0x30, kPatternWin10x64,
+                 sizeof(kPatternWin10x64)));
+  if (!found) {
+    return STATUS_PROCEDURE_NOT_FOUND;
+  }
+
+  found += sizeof(kPatternWin10x64);
+  HYPERPLATFORM_LOG_DEBUG("Found a hard coded PTE_BASE at %p", found);
+
+  const auto pte_base = *reinterpret_cast<ULONG_PTR *>(found);
+  const auto index = (pte_base >> kUtilpPxiShift) & kUtilpPxiMask;
+  const auto pde_base = pte_base | (index << kUtilpPpiShift);
+  const auto ppe_base = pde_base | (index << kUtilpPdiShift);
+  const auto pxe_base = ppe_base | (index << kUtilpPtiShift);
+
+  g_utilp_pxe_base = static_cast<ULONG_PTR>(pxe_base);
+  g_utilp_ppe_base = static_cast<ULONG_PTR>(ppe_base);
+  g_utilp_pde_base = static_cast<ULONG_PTR>(pde_base);
+  g_utilp_pte_base = static_cast<ULONG_PTR>(pte_base);
+
+  g_utilp_pxi_shift = kUtilpPxiShift;
+  g_utilp_ppi_shift = kUtilpPpiShift;
+  g_utilp_pdi_shift = kUtilpPdiShift;
+  g_utilp_pti_shift = kUtilpPtiShift;
+
+  g_utilp_pdi_mask = kUtilpPdiMask;
+  g_utilp_pti_mask = kUtilpPtiMask;
+  g_utilp_pxi_mask = kUtilpPxiMask;
+  g_utilp_ppi_mask = kUtilpPpiMask;
+  return status;
 }
 
 // Locates RtlPcToFileHeader
@@ -443,19 +493,16 @@ _Use_decl_annotations_ bool UtilIsAccessibleAddress(void *address) {
     return false;
   }
 
-#if defined(_AMD64_)
-  const auto pxe = UtilpAddressToPxe(address);
-  const auto ppe = UtilpAddressToPpe(address);
-  if (!pxe->valid || !ppe->valid) {
-    return false;
+  if (IsX64()) {
+    const auto pxe = UtilpAddressToPxe(address);
+    const auto ppe = UtilpAddressToPpe(address);
+    if (!pxe->valid || !ppe->valid) {
+      return false;
+    }
   }
-#endif
 
-  const auto is_x86_pae = UtilIsX86Pae();
-  const auto pde =
-      (is_x86_pae) ? UtilpAddressToPdePAE(address) : UtilpAddressToPde(address);
-  const auto pte =
-      (is_x86_pae) ? UtilpAddressToPtePAE(address) : UtilpAddressToPte(address);
+  const auto pde = UtilpAddressToPde(address);
+  const auto pte = UtilpAddressToPte(address);
   if (!pde->valid) {
     return false;
   }
@@ -477,108 +524,40 @@ _Use_decl_annotations_ static bool UtilpIsCanonicalFormAddress(void *address) {
                          reinterpret_cast<ULONG64>(address));
 }
 
-// Virtual Address Interpretation For Handling PTEs
-//
-// -- On x64
-// Sign extension                     16 bits
-// Page map level 4 selector           9 bits
-// Page directory pointer selector     9 bits
-// Page directory selector             9 bits
-// Page table selector                 9 bits
-// Byte within page                   12 bits
-// 11111111 11111111 11111000 10000000 00000011 01010011 00001010 00011000
-// ^^^^^^^^ ^^^^^^^^ ~~~~~~~~ ~^^^^^^^ ^^~~~~~~ ~~~^^^^^ ^^^^~~~~ ~~~~~~~~
-// Sign extension    PML4      PDPT      PD        PT        Offset
-//
-// -- On x86(PAE)
-// Page directory pointer selector     2 bits
-// Page directory selector             9 bits
-// Page table selector                 9 bits
-// Byte within page                   12 bits
-// 10 000011011 000001101 001001110101
-// ^^ ~~~~~~~~~ ^^^^^^^^^ ~~~~~~~~~~~~
-// PDPT PD      PT        Offset
-//
-// -- On x86 and ARM
-// Page directory selector            10 bits
-// Page table selector                10 bits
-// Byte within page                   12 bits
-// 1000001101 1000001101 001001110101
-// ~~~~~~~~~~ ^^^^^^^^^^ ~~~~~~~~~~~~
-// PD         PT         Offset
-//
-//
-//                                   x64   x86(PAE)  x86   ARM
-// Page map level 4 selector           9          -    -     -
-// Page directory pointer selector     9          2    -     -
-// Page directory selector             9          9   10    10
-// Page table selector                 9          9   10    10
-// Byte within page                   12         12   12    12
-//
-// 6666555555555544444444443333333333222222222211111111110000000000
-// 3210987654321098765432109876543210987654321098765432109876543210
-// ----------------------------------------------------------------
-// aaaaaaaaaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeeeeeeffffffffffff  x64
-// ................................ccdddddddddeeeeeeeeeffffffffffff  x86(PAE)
-// ................................ddddddddddeeeeeeeeeeffffffffffff  x86
-// ................................ddddddddddeeeeeeeeeeffffffffffff  ARM
-//
-// a = Sign extension, b = PML4, c = PDPT, d = PD, e = PT, f = Offset
-
-#if defined(_AMD64_)
 // Return an address of PXE
 _Use_decl_annotations_ static HardwarePte *UtilpAddressToPxe(
     const void *address) {
   const auto addr = reinterpret_cast<ULONG_PTR>(address);
-  const auto pxe_index = (addr >> kUtilpPxiShift) & kUtilpPxiMask;
+  const auto pxe_index = (addr >> g_utilp_pxi_shift) & g_utilp_pxi_mask;
   const auto offset = pxe_index * sizeof(HardwarePte);
-  return reinterpret_cast<HardwarePte *>(kUtilpPxeBase + offset);
+  return reinterpret_cast<HardwarePte *>(g_utilp_pxe_base + offset);
 }
 
 // Return an address of PPE
 _Use_decl_annotations_ static HardwarePte *UtilpAddressToPpe(
     const void *address) {
   const auto addr = reinterpret_cast<ULONG_PTR>(address);
-  const auto ppe_index = (addr >> kUtilpPpiShift) & kUtilpPpiMask;
+  const auto ppe_index = (addr >> g_utilp_ppi_shift) & g_utilp_ppi_mask;
   const auto offset = ppe_index * sizeof(HardwarePte);
-  return reinterpret_cast<HardwarePte *>(kUtilpPpeBase + offset);
+  return reinterpret_cast<HardwarePte *>(g_utilp_ppe_base + offset);
 }
-#endif
 
 // Return an address of PDE
 _Use_decl_annotations_ static HardwarePte *UtilpAddressToPde(
     const void *address) {
   const auto addr = reinterpret_cast<ULONG_PTR>(address);
-  const auto pde_index = (addr >> kUtilpPdiShift) & kUtilpPdiMask;
+  const auto pde_index = (addr >> g_utilp_pdi_shift) & g_utilp_pdi_mask;
   const auto offset = pde_index * sizeof(HardwarePte);
-  return reinterpret_cast<HardwarePte *>(kUtilpPdeBase + offset);
+  return reinterpret_cast<HardwarePte *>(g_utilp_pde_base + offset);
 }
 
 // Return an address of PTE
 _Use_decl_annotations_ static HardwarePte *UtilpAddressToPte(
     const void *address) {
   const auto addr = reinterpret_cast<ULONG_PTR>(address);
-  const auto pte_index = (addr >> kUtilpPtiShift) & kUtilpPtiMask;
+  const auto pte_index = (addr >> g_utilp_pti_shift) & g_utilp_pti_mask;
   const auto offset = pte_index * sizeof(HardwarePte);
-  return reinterpret_cast<HardwarePte *>(kUtilpPteBase + offset);
-}
-
-// Return an address of PDE for PAE enabled x86
-_Use_decl_annotations_ static HardwarePte *UtilpAddressToPdePAE(
-    const void *address) {
-  const auto addr = reinterpret_cast<ULONG_PTR>(address);
-  const auto pde_index = (addr >> kUtilpPdiShiftPae) & kUtilpPdiMaskPae;
-  const auto offset = pde_index * sizeof(HardwarePteX86Pae);
-  return reinterpret_cast<HardwarePte *>(kUtilpPdeBasePae + offset);
-}
-
-// Return an address of PTE for PAE enabled x86
-_Use_decl_annotations_ static HardwarePte *UtilpAddressToPtePAE(
-    const void *address) {
-  const auto addr = reinterpret_cast<ULONG_PTR>(address);
-  const auto pte_index = (addr >> kUtilpPtiShiftPae) & kUtilpPtiMaskPae;
-  const auto offset = pte_index * sizeof(HardwarePteX86Pae);
-  return reinterpret_cast<HardwarePte *>(kUtilpPteBasePae + offset);
+  return reinterpret_cast<HardwarePte *>(g_utilp_pte_base + offset);
 }
 
 // VA -> PA
@@ -804,7 +783,7 @@ _Use_decl_annotations_ void UtilLoadPdptes(ULONG_PTR cr3_value) {
   // Gets PDPTEs fomr CR3
   PdptrRegister pd_pointers[4] = {};
   for (auto i = 0ul; i < 4; ++i) {
-    const auto pd_addr = kUtilpPdeBasePae + i * PAGE_SIZE;
+    const auto pd_addr = g_utilp_pde_base + i * PAGE_SIZE;
     pd_pointers[i].fields.present = true;
     pd_pointers[i].fields.page_directory_pa =
         UtilPfnFromVa(reinterpret_cast<void *>(pd_addr));
---
+++ HyperPlatform/util_page_constants.h
@@ -0,0 +1,140 @@
+// Copyright (c) 2015-2016, tandasat. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+/// @file
+/// Defines page table related constants
+///
+/// This file defienes platform depedent constants and is included only from a
+/// function where initializes g_utilp_p*e_base, g_utilp_p*i_shift and
+/// g_utilp_p*i_mask global variables.
+
+#ifndef HYPERPLATFORM_UTIL_CONSTANT_H_
+#define HYPERPLATFORM_UTIL_CONSTANT_H_
+
+// Virtual Address Interpretation For Handling PTEs
+//
+// -- On x64
+// Sign extension                     16 bits
+// Page map level 4 selector           9 bits
+// Page directory pointer selector     9 bits
+// Page directory selector             9 bits
+// Page table selector                 9 bits
+// Byte within page                   12 bits
+// 11111111 11111111 11111000 10000000 00000011 01010011 00001010 00011000
+// ^^^^^^^^ ^^^^^^^^ ~~~~~~~~ ~^^^^^^^ ^^~~~~~~ ~~~^^^^^ ^^^^~~~~ ~~~~~~~~
+// Sign extension    PML4      PDPT      PD        PT        Offset
+//
+// -- On x86(PAE)
+// Page directory pointer selector     2 bits
+// Page directory selector             9 bits
+// Page table selector                 9 bits
+// Byte within page                   12 bits
+// 10 000011011 000001101 001001110101
+// ^^ ~~~~~~~~~ ^^^^^^^^^ ~~~~~~~~~~~~
+// PDPT PD      PT        Offset
+//
+// -- On x86 and ARM
+// Page directory selector            10 bits
+// Page table selector                10 bits
+// Byte within page                   12 bits
+// 1000001101 1000001101 001001110101
+// ~~~~~~~~~~ ^^^^^^^^^^ ~~~~~~~~~~~~
+// PD         PT         Offset
+//
+//
+//                                   x64   x86(PAE)  x86   ARM
+// Page map level 4 selector           9          -    -     -
+// Page directory pointer selector     9          2    -     -
+// Page directory selector             9          9   10    10
+// Page table selector                 9          9   10    10
+// Byte within page                   12         12   12    12
+//
+// 6666555555555544444444443333333333222222222211111111110000000000
+// 3210987654321098765432109876543210987654321098765432109876543210
+// ----------------------------------------------------------------
+// aaaaaaaaaaaaaaaabbbbbbbbbcccccccccdddddddddeeeeeeeeeffffffffffff  x64
+// ................................ccdddddddddeeeeeeeeeffffffffffff  x86(PAE)
+// ................................ddddddddddeeeeeeeeeeffffffffffff  x86
+// ................................ddddddddddeeeeeeeeeeffffffffffff  ARM
+//
+// a = Sign extension, b = PML4, c = PDPT, d = PD, e = PT, f = Offset
+
+#if defined(_AMD64_)
+
+// Base addresses of page structures. Use !pte to obtain them.
+static auto kUtilpPxeBase = 0xfffff6fb7dbed000ull;
+static auto kUtilpPpeBase = 0xfffff6fb7da00000ull;
+static auto kUtilpPdeBase = 0xfffff6fb40000000ull;
+static auto kUtilpPteBase = 0xfffff68000000000ull;
+
+// Get the highest 25 bits
+static const auto kUtilpPxiShift = 39ull;
+
+// Get the highest 34 bits
+static const auto kUtilpPpiShift = 30ull;
+
+// Get the highest 43 bits
+static const auto kUtilpPdiShift = 21ull;
+
+// Get the highest 52 bits
+static const auto kUtilpPtiShift = 12ull;
+
+// Use  9 bits; 0b0000_0000_0000_0000_0000_0000_0001_1111_1111
+static const auto kUtilpPxiMask = 0x1ffull;
+
+// Use 18 bits; 0b0000_0000_0000_0000_0011_1111_1111_1111_1111
+static const auto kUtilpPpiMask = 0x3ffffull;
+
+// Use 27 bits; 0b0000_0000_0111_1111_1111_1111_1111_1111_1111
+static const auto kUtilpPdiMask = 0x7ffffffull;
+
+// Use 36 bits; 0b1111_1111_1111_1111_1111_1111_1111_1111_1111
+static const auto kUtilpPtiMask = 0xfffffffffull;
+
+#elif defined(_X86_)
+
+// Base addresses of page structures. Use !pte to obtain them.
+static auto kUtilpPdeBase = 0xc0300000;
+static auto kUtilpPteBase = 0xc0000000;
+
+// Get the highest 10 bits
+static const auto kUtilpPdiShift = 22;
+
+// Get the highest 20 bits
+static const auto kUtilpPtiShift = 12;
+
+// Use 10 bits; 0b0000_0000_0000_0000_0000_0000_0011_1111_1111
+static const auto kUtilpPdiMask = 0x3ff;
+
+// Use 20 bits; 0b0000_0000_0000_0000_1111_1111_1111_1111_1111
+static const auto kUtilpPtiMask = 0xfffff;
+
+// unused but defined to compile without ifdef
+
+static auto kUtilpPxeBase = 0;
+static auto kUtilpPpeBase = 0;
+static const auto kUtilpPxiShift = 0;
+static const auto kUtilpPpiShift = 0;
+static const auto kUtilpPxiMask = 0;
+static const auto kUtilpPpiMask = 0;
+
+#endif
+
+// Base addresses of page structures. Use !pte to obtain them.
+static const auto kUtilpPdeBasePae = 0xc0600000;
+static const auto kUtilpPteBasePae = 0xc0000000;
+
+// Get the highest 11 bits
+static const auto kUtilpPdiShiftPae = 21;
+
+// Get the highest 20 bits
+static const auto kUtilpPtiShiftPae = 12;
+
+// Use 11 bits; 0b0000_0000_0000_0000_0000_0000_0111_1111_1111
+static const auto kUtilpPdiMaskPae = 0x7ff;
+
+// Use 20 bits; 0b0000_0000_0000_0000_1111_1111_1111_1111_1111
+static const auto kUtilpPtiMaskPae = 0xfffff;
+
+#endif  // HYPERPLATFORM_UTIL_H_
