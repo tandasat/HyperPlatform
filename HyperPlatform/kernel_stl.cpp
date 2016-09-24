31e2ff6ff9a38db8c870c186dfd12f29d13dc11a
Satoshi Tanda <tanda.sat@gmail.com>
2016-09-24 08:54:18 -0700

    fix SAL in kernel_stl.cpp
    Signed-off-by: Satoshi Tanda <tanda.sat@gmail.com>

--- HyperPlatform/kernel_stl.cpp
+++ HyperPlatform/kernel_stl.cpp
@@ -5,7 +5,9 @@
 /// @file
 /// Implements code to use STL in a driver project
 
-#include "kernel_stl.h"
+#include <fltKernel.h>
+#undef _HAS_EXCEPTIONS
+#define _HAS_EXCEPTIONS 0
 
 // See common.h for details
 #pragma prefast(disable : 30030)
@@ -44,8 +46,8 @@ static const ULONG kKstlpPoolTag = 'LTSK';
 //
 
 // An alternative implementation of a C++ exception handler. Issues a bug check.
-_Use_decl_annotations_ DECLSPEC_NORETURN void KernelStlRaiseException(
-    ULONG bug_check_code) {
+DECLSPEC_NORETURN static void KernelStlpRaiseException(
+    _In_ ULONG bug_check_code) {
   KdBreakPoint();
 #pragma warning(push)
 #pragma warning(disable : 28159)
@@ -53,63 +55,57 @@ _Use_decl_annotations_ DECLSPEC_NORETURN void KernelStlRaiseException(
 #pragma warning(pop)
 }
 
-// Followings are definitions of functions needed to link successfully.
-
-/*_Use_decl_annotations_*/
 DECLSPEC_NORETURN void __cdecl _invalid_parameter_noinfo_noreturn() {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
 
 namespace std {
 
-/*_Use_decl_annotations_*/ DECLSPEC_NORETURN void __cdecl _Xbad_alloc() {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+DECLSPEC_NORETURN void __cdecl _Xbad_alloc() {
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
-_Use_decl_annotations_ DECLSPEC_NORETURN void __cdecl _Xinvalid_argument(
-    _In_z_ const char *) {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+DECLSPEC_NORETURN void __cdecl _Xinvalid_argument(_In_z_ const char *) {
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
-_Use_decl_annotations_ DECLSPEC_NORETURN void __cdecl _Xlength_error(
-    _In_z_ const char *) {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+DECLSPEC_NORETURN void __cdecl _Xlength_error(_In_z_ const char *) {
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
-_Use_decl_annotations_ DECLSPEC_NORETURN void __cdecl _Xout_of_range(
-    _In_z_ const char *) {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+DECLSPEC_NORETURN void __cdecl _Xout_of_range(_In_z_ const char *) {
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
-_Use_decl_annotations_ DECLSPEC_NORETURN void __cdecl _Xoverflow_error(
-    _In_z_ const char *) {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+DECLSPEC_NORETURN void __cdecl _Xoverflow_error(_In_z_ const char *) {
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
-_Use_decl_annotations_ DECLSPEC_NORETURN void __cdecl _Xruntime_error(
-    _In_z_ const char *) {
-  KernelStlRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
+DECLSPEC_NORETURN void __cdecl _Xruntime_error(_In_z_ const char *) {
+  KernelStlpRaiseException(KMODE_EXCEPTION_NOT_HANDLED);
 }
 
 }  // namespace std
 
 // An alternative implementation of the new operator
-_Use_decl_annotations_ void *__cdecl operator new(size_t size) {
+_IRQL_requires_max_(DISPATCH_LEVEL) void *__cdecl operator new(
+    _In_ size_t size) {
   if (size == 0) {
     size = 1;
   }
 
-  void *p = ExAllocatePoolWithTag(NonPagedPool, size, kKstlpPoolTag);
+  const auto p = ExAllocatePoolWithTag(NonPagedPool, size, kKstlpPoolTag);
   if (!p) {
-    KernelStlRaiseException(MUST_SUCCEED_POOL_EMPTY);
+    KernelStlpRaiseException(MUST_SUCCEED_POOL_EMPTY);
   }
   return p;
 }
 
 // An alternative implementation of the new operator
-_Use_decl_annotations_ void __cdecl operator delete(void *p) {
+_IRQL_requires_max_(DISPATCH_LEVEL) void __cdecl operator delete(_In_ void *p) {
   if (p) {
     ExFreePoolWithTag(p, kKstlpPoolTag);
   }
 }
 
 // An alternative implementation of the new operator
-/*_Use_decl_annotations_*/ void __cdecl operator delete(void *p, size_t size) {
+_IRQL_requires_max_(DISPATCH_LEVEL) void __cdecl operator delete(
+    _In_ void *p, _In_ size_t size) {
   UNREFERENCED_PARAMETER(size);
   if (p) {
     ExFreePoolWithTag(p, kKstlpPoolTag);
@@ -117,9 +113,11 @@ _Use_decl_annotations_ void __cdecl operator delete(void *p) {
 }
 
 // An alternative implementation of __stdio_common_vsprintf_s
-_Use_decl_annotations_ EXTERN_C int __cdecl __stdio_common_vsprintf_s(
-    unsigned __int64 _Options, char *_Buffer, size_t _BufferCount,
-    char const *_Format, _locale_t _Locale, va_list _ArgList) {
+_Success_(return >= 0) EXTERN_C inline int __cdecl __stdio_common_vsprintf_s(
+    _In_ unsigned __int64 _Options, _Out_writes_z_(_BufferCount) char *_Buffer,
+    _In_ size_t _BufferCount,
+    _In_z_ _Printf_format_string_params_(2) char const *_Format,
+    _In_opt_ _locale_t _Locale, va_list _ArgList) {
   UNREFERENCED_PARAMETER(_Options);
   UNREFERENCED_PARAMETER(_Locale);
 
@@ -137,9 +135,12 @@ _Use_decl_annotations_ EXTERN_C int __cdecl __stdio_common_vsprintf_s(
 }
 
 // An alternative implementation of __stdio_common_vswprintf_s
-_Use_decl_annotations_ EXTERN_C int __cdecl __stdio_common_vswprintf_s(
-    unsigned __int64 _Options, wchar_t *_Buffer, size_t _BufferCount,
-    wchar_t const *_Format, _locale_t _Locale, va_list _ArgList) {
+_Success_(return >= 0) _Check_return_opt_ EXTERN_C
+    inline int __cdecl __stdio_common_vswprintf_s(
+        _In_ unsigned __int64 _Options,
+        _Out_writes_z_(_BufferCount) wchar_t *_Buffer, _In_ size_t _BufferCount,
+        _In_z_ _Printf_format_string_params_(2) wchar_t const *_Format,
+        _In_opt_ _locale_t _Locale, va_list _ArgList) {
   UNREFERENCED_PARAMETER(_Options);
   UNREFERENCED_PARAMETER(_Locale);
 
