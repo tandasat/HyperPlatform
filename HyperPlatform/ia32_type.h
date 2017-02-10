849308c25f07343ceb50d94152479eacd3437442
Satoshi Tanda <tanda.sat@gmail.com>
2017-02-10 06:54:25 -0800

    update copyright
    Signed-off-by: Satoshi Tanda <tanda.sat@gmail.com>

--- HyperPlatform/ia32_type.h
+++ HyperPlatform/ia32_type.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016, tandasat. All rights reserved.
+// Copyright (c) 2015-2017, Satoshi Tanda. All rights reserved.
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 
